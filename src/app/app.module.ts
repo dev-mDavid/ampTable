14282321a3f04afd9ff7353f0ec9ddbde3f11d81
mDavid <dev.mdavid@gmail.com>
2020-10-21 15:30:46 -0700

    build: generate core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { CoreModule } from "./core/core.module";
 import { SharedModule } from "./shared/shared.module";
 @NgModule({
   declarations: [
@@ -12,6 +13,7 @@ import { SharedModule } from "./shared/shared.module";
   imports: [
     BrowserModule,
     AppRoutingModule,
+    CoreModule,
     SharedModule
   ],
   providers: [],
