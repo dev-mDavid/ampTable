fd55184fbe20f63d4f5d6bea78e68d687311c6af
mDavid <dev.mdavid@gmail.com>
2020-10-21 16:37:29 -0700

    fix: shared module available to demo-page module

--- src/app/demo-page/demo-page.module.ts
+++ src/app/demo-page/demo-page.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { SharedModule } from "../shared/shared.module";
 import { DemoPageRoutingModule } from './demo-page-routing.module';
-import { DemoPageComponent } from './demo-page.component';
 
+import { DemoPageComponent } from './demo-page.component';
 
 @NgModule({
   declarations: [DemoPageComponent],
   imports: [
     CommonModule,
+    SharedModule,
     DemoPageRoutingModule
   ]
 })
