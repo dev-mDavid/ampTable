a611beae876dad8ed5437b79f776d0f0f34f3f53
mDavid <dev.mdavid@gmail.com>
2020-10-21 15:49:41 -0700

    feat: generate and configure nav-shell

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,11 @@ import { AppComponent } from './app.component';
 
 import { CoreModule } from "./core/core.module";
 import { SharedModule } from "./shared/shared.module";
+import { NavShellComponent } from './nav-shell/nav-shell.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavShellComponent
   ],
   imports: [
     BrowserModule,
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
 const components = [
 
 ]
 
 const modules = [
   CommonModule,
+  MatToolbarModule,
+  MatButtonModule,
+  MatIconModule
 ]
 
 
