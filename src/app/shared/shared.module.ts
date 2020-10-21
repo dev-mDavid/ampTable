31c42c9dcbefa0b209dd32f285a4b4f6dc0560b1
mDavid <dev.mdavid@gmail.com>
2020-10-21 15:27:11 -0700

    build: generate and configerue shared module

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+const components = [
+
+]
+
+const modules = [
+  CommonModule,
+]
+
+
+@NgModule({
+  declarations: [...components],
+  imports: [...modules],
+  exports: [...components, ...modules]
+})
+export class SharedModule { }
