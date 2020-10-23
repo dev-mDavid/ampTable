d30d198eba0f5cbc42175b4ea42021ade2ae3d1c
mDavid <dev.mdavid@gmail.com>
2020-10-23 12:32:29 -0700

    build: import HttpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 
 import { CoreModule } from "./core/core.module";
 import { SharedModule } from "./shared/shared.module";
@@ -15,6 +16,7 @@ import { NavShellComponent } from './nav-shell/nav-shell.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     CoreModule,
     SharedModule
   ],
