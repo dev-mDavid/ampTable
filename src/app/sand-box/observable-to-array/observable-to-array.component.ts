e224b9f431969b7209eadd92f1a08b956fe46f74
mDavid <dev.mdavid@gmail.com>
2020-10-24 12:53:33 -0700

    docs: sandbox a component to turn observables into an array in the TS

--- src/app/sand-box/observable-to-array/observable-to-array.component.ts
+++ src/app/sand-box/observable-to-array/observable-to-array.component.ts
@@ -1,20 +1,55 @@
+/* 
+  Problem:  in amp-table, I need drag & drop the columns WHILE also remembering the state of each column (order/position and width). 
+  The main issue is a data-type problem. The store emits in observables, the drag & drop method needs to pass in an array.
+
+  Solution/Approach: for this sandbox component, turn observables from the store into an array
+
+  Solved! (what I was missing): I needed to subscribe in the TS and not in the HTML. By doing this, I was able to unwrap the observables in the TS, allowing me to transform it into an array.
+*/
+
 import { Component, OnInit } from '@angular/core';
+
+import { Store } from "@ngrx/store";
+import { Observable, of, Subscription } from "rxjs";
+import { toArray } from "rxjs/operators";
+import * as fromCol from "../../core/col.reducer"
+import * as actions from "../../core/col.action"
+
+import { Col } from "../../core/col.reducer";
 
 @Component({
   selector: 'app-observable-to-array',
   template: `
-    <p>
-      observable-to-array works!
-    </p>
+    <h1>
+      Observable to Array
+    </h1>
+
+    <div *ngFor="let col of cols">
+      {{ col.name }}
+    </div>
   `,
   styles: [
   ]
 })
-export class ObservableToArrayComponent implements OnInit {
+export class ObservableToArrayComponent{
 
-  constructor() { }
+  cols: Array<any>;
+  subscription: Subscription;
 
-  ngOnInit(): void {
+  constructor(
+    private store: Store<fromCol.State>
+    ) { }
+
+  ngOnInit() {
+
+    this.subscription = this.store.select(fromCol.selectAll).subscribe(cols => {
+      this.cols = cols
+    })        
+
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe()
   }
 
 }
