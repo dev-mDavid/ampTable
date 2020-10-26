f793845c0ff64b9be2f87badef8977833d27beab
mDavid <dev.mdavid@gmail.com>
2020-10-26 11:25:51 -0700

    feat: add functionality to resizing sandbox component

--- src/app/sand-box/resizing-page/resizing-page.component.ts
+++ src/app/sand-box/resizing-page/resizing-page.component.ts
@@ -1,20 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-resizing-page',
+  selector: 'sb-resizing-page',
   template: `
     <h1>
       Resizing Page Works!
     </h1>
+    <div class="box-container">
+      <span>600px</span>
+      <span>400px</span>
+      <sb-dynamic-resizing
+        [width]="300"
+        [height]="150"
+        [left]="100"
+        [top]="100">
+    </sb-dynamic-resizing>
+    </div>
   `,
-  styles: [
-  ]
+  styleUrls: ['./resizing-page.component.scss']
 })
-export class ResizingPageComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class ResizingPageComponent {
+ title = 'angular-resizable-draggable';
 
 }
